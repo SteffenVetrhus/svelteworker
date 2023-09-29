9003ed5333d16ad34d7a01ce10f24e40ac808fc1
Steffen Vetrhus <steffen.vetrhus@swipload.no>
2023-09-29 07:37:12 +0200

    g

--- src/routes/shipper/+page.svelte
+++ src/routes/shipper/+page.svelte
@@ -1,1 +1,11 @@
+<script lang="ts">
+  import { updated } from "$app/stores";
+
+  const checkforUpdate = () => {
+    updated.subscribe((e) => console.log(e));
+    updated.check();
+  };
+</script>
+
 <h1>Shippsser</h1>
+<button on:click={() => checkforUpdate()}>Btn</button>
