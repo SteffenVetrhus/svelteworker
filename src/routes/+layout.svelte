906e65f4ffa7bbcc21d9b1099b922c4966e812dd
Steffen Vetrhus <steffen.vetrhus@swipload.no>
2023-09-29 15:49:51 +0200

    r

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,10 @@
-<a href="/admin">Admin</a>
-<a href="/carrier">Carrier</a>
-<a href="/shipper">Shipper</a>
-<a href="/admin">Admin</a>
+<script lang="ts">
+  import { updated } from "$app/stores";
+</script>
+
+<a data-sveltekit-reload={$updated ? "" : "off"} href="/admin">Admin</a>
+<a data-sveltekit-reload={$updated ? "" : "off"} href="/carrier">Carrier</a>
+<a data-sveltekit-reload={$updated ? "" : "off"} href="/shipper">Shipper</a>
+<a data-sveltekit-reload={$updated ? "" : "off"} href="/admin">Admin</a>
 
 <slot />
